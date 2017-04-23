29dd423bca68f2c5aa2c0d9cd5900cb42420c453
Ainor Syahrizal <macintorch@gmail.com>
2017-04-23 15:04:18 +0800

    apply CursorAdapter to display data from db

--- app/src/main/java/ainor/com/my/sqlitedemo/LoadActivity.java
+++ app/src/main/java/ainor/com/my/sqlitedemo/LoadActivity.java
@@ -1,10 +1,15 @@
 package ainor.com.my.sqlitedemo;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CursorAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 public class LoadActivity extends AppCompatActivity {
+
+    ProductHandler productHandler;
 
     ListView productsListView;
 
@@ -14,5 +19,18 @@ public class LoadActivity extends AppCompatActivity {
         setContentView(R.layout.activity_load);
 
         productsListView = (ListView) findViewById(R.id.productsListView);
+
+        //get into cursor
+
+        productHandler = new ProductHandler(this);
+        Cursor cursor = productHandler.loadAllProducts();
+        CursorAdapter cursorAdapter = new SimpleCursorAdapter(
+            this, R.layout.product_layout,
+                cursor,
+                new String[]{"_id","name","quantity"},
+                new int[]{R.id.productIdTextView, R.id.productNameTextView, R.id.productQuantityTextView}
+        );
+
+        productsListView.setAdapter(cursorAdapter);
     }
 }
